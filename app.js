71d87fb6e81f2386d8d43297135ff157ec110d5f
notPHPuser <artem2005dm@mail.ru>
2024-08-18 12:14:52 +0300

    нужно делать дальше

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
 function GoToService() {
-  const services = document.querySelector('.h1_services');
+  const services = document.querySelector('.title_we_do');
   services.scrollIntoView({ behavior: 'smooth' });
 }
 
-// const btnDezin = document.querySelector('.btn-dezin');
-// const btnRashod = document.querySelector('.btn-rashod');
-// const btnReagent = document.querySelector('.btn-reagent');
-// const dezinDiv = document.querySelector('.forFirst');
-// const rashodDiv = document.querySelector('.forSecond');
-// const reagentDiv = document.querySelector('.forThird');
-
-// btnDezin.addEventListener('click', () => {
-//   dezinDiv.classList.remove('hidden');
-//   dezinDiv.classList.add('visible');
-//   rashodDiv.classList.add('hidden');
-//   reagentDiv.classList.add('hidden');
-//   console.log(1);
-// });
-
-// btnRashod.addEventListener('click', () => {
-//   rashodDiv.classList.remove('hidden');
-//   rashodDiv.classList.add('visible');
-//   dezinDiv.classList.add('hidden');
-//   reagentDiv.classList.add('hidden');
-//   console.log(2);
-// });
-
-// btnReagent.addEventListener('click', () => {
-//   reagentDiv.classList.remove('hidden');
-//   reagentDiv.classList.add('visible');
-//   dezinDiv.classList.add('hidden');
-//   rashodDiv.classList.add('hidden');
-//   console.log(3);
-// });
+document.addEventListener('DOMContentLoaded', function () {
+  const mainPhoto = document.querySelector('.mainPhoto');
+  mainPhoto.classList.add('show');
+});
