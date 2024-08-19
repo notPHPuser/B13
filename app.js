4c830cbb0a93f5a58294d0ea8cdf8a586f84f6b5
notPHPuser <artem2005dm@mail.ru>
2024-08-20 00:00:45 +0300

    1

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ document.addEventListener('DOMContentLoaded', function () {
   const mainPhoto = document.querySelector('.mainPhoto');
   mainPhoto.classList.add('show');
 });
+
+let currentIndex = 0;
+const images = document.querySelectorAll('.img_about');
+
+function showNextImage() {
+  images[currentIndex].classList.remove('active');
+
+  currentIndex = (currentIndex + 1) % images.length;
+
+  images[currentIndex].classList.add('active');
+}
+
+images[currentIndex].classList.add('active');
+
+setInterval(showNextImage, 3000);
